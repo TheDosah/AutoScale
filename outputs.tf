c243487cad7f52a04b42335b516a00b5d373d810
David Aguirre Huelves <david.aguirre.huelves@oracle.com>
2022-02-18 11:37:36 +0100

    AA

--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "aipac_id" {
-  description = "OCID of created analytics instance private access channel."
-  value       = oci_analytics_analytics_instance_private_access_channel.this.id
-}
--- version.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    oci = {
-      source  = "hashicorp/oci"
-      version = "4.44.0"
-    }
-  }
-  required_version = ">=1.0"
-}
